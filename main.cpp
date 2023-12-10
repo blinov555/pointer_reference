4ac36e084c4d1a6c761a71e6d887b29d96ac079e
blinov555 <60940075+blinov555@users.noreply.github.com>
2023-12-10 23:38:16 +0500

    ptr/ref

---
+++ main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using namespace std;
+
+struct sample {
+  int a {1};
+  int b {2};
+}param;
+
+sample fooPtr(sample *);
+sample &fooRef(sample &);
+
+int main() {
+
+  cout << param.a << " " << param.b << "\n";
+
+  sample *ptr;
+  ptr = &param;
+
+  param = fooPtr(ptr);
+
+  cout << param.a << " " << param.b << "\n";
+
+  param = fooRef(param);
+
+  cout << param.a << " " << param.b << "\n";
+
+  return 0;
+}
+
+sample fooPtr(sample *p){
+
+  p->a = 11;
+  p->b = 22;
+
+  return *p;
+}
+
+sample &fooRef(sample &r){
+
+  r.a = 111;
+  r.b = 222;
+
+  return r;
+}
